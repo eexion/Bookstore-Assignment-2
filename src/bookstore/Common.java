a7b7d8d55aae7b2c6d2e7ed3ce62bfd16b4d80d9
Hideki <dekiyana@gmail.com>
2018-06-07 14:10:11 +0800

    Common class super class
    All other object classes will extend this class

--- src/bookstore/Common.java
+++ src/bookstore/Common.java
@@ -1,5 +1,123 @@
 package bookstore;
+import java.util.ArrayList;
 
-public class Common {
-	//common object variables getters and setters
+import interfaces.*;
+public class Common implements ICommon {
+	private String name;
+	private String description;
+	private int inventory;
+	private String type;
+	private int quantity;
+	private String status;
+	private double price;
+	private double total;
+	private String rentalPrice;
+	private final static double TAX = 0.1;
+	public Common() {
+		this("","",0, "", 2, "");
+	}
+	public Common(String name, String description, int inventory, String type, int quantity, String status) {
+		this.name = name;
+		this.description =description;
+		this.inventory = inventory;
+		this.type = type;
+		this.quantity = quantity;
+		this.status = status;
+	}
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		this.name = name;
+	}
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	@Override
+	public void setInventory(int inventory) {
+		// TODO Auto-generated method stub
+		this.inventory = inventory;
+	}
+	@Override
+	public int getInventory() {
+		// TODO Auto-generated method stub
+		return this.inventory;
+	}
+	@Override
+	public void setDescription(String description) {
+		// TODO Auto-generated method stub
+		this.description = description;
+	}
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return this.description;
+	}
+	@Override
+	public void setType(String type) {
+		// TODO Auto-generated method stub
+		this.type = type;
+	}
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return this.type;
+	}
+	@Override
+	public void setQuantity(int quantity) {
+		// TODO Auto-generated method stub
+		this.quantity = quantity;
+	}
+	@Override
+	public int getQuantity() {
+		// TODO Auto-generated method stub
+		return this.quantity;
+	}
+	@Override
+	public void setStatus(String status) {
+		// TODO Auto-generated method stub
+		this.status = status;
+	}
+	@Override
+	public String getStatus() {
+		// TODO Auto-generated method stub
+		return this.status;
+	}
+	@Override
+	public void setPrice(double price) {
+		// TODO Auto-generated method stub
+		this.price = price;
+	}
+	@Override
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return this.price;
+	}
+	@Override
+	public double calculateTotal(ArrayList<Common> products) {
+		// TODO Auto-generated method stub
+		double totalBeforTax = 0.0;
+		for(Common item : products) {
+			totalBeforTax = totalBeforTax + (item.getPrice() * item.getQuantity());
+		}
+		this.total = totalBeforTax + getTax(totalBeforTax);
+		return this.total;
+	}
+	@Override
+	public double getTax(double total) {
+		// TODO Auto-generated method stub
+		double tax = total * TAX;
+		return tax;
+	}
+	@Override
+	public void setRentalPrice(String rentalPrice) {
+		// TODO Auto-generated method stub
+		this.rentalPrice = rentalPrice;
+	}
+	@Override
+	public String getRentalPrice() {
+		// TODO Auto-generated method stub
+		return this.rentalPrice;
+	}
 }
