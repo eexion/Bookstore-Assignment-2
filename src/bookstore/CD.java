ead5f2e488963d3fea765245fdebf3df00da9eb0
leonardho900 <39939576+leonardho900@users.noreply.github.com>
2018-06-08 14:01:52 +0800

    cd

--- src/bookstore/CD.java
+++ src/bookstore/CD.java
@@ -1,7 +1,55 @@
 package bookstore;
 
 public class CD {
-	
-		
-
-}
+		String title;
+	    int duration;
+	    String director;
+	    String genre;
+	    int yearmade;
+	    
+	    public CD(){
+	    	title="";
+	    	duration=0;
+	    	director="";
+	    	genre="";
+	    	yearmade=0;	
+	    } 
+	    
+	   public CD(String title,int duration,String director,String genre,int yearmade){
+		   this.title=title;
+		   this.duration=duration;
+		   this.director=director;
+		   this.genre=genre;
+		   this.yearmade=yearmade;
+	   }
+	   public void settitle(String title){
+		   this.title=title;
+	   }
+	   public String gettitle(){
+		   return title;
+	   }
+	   public void setduration(int duration){
+		   this.duration=duration;
+	   }
+	   public int getduration(){
+		   return duration;
+	   }
+	   public void setdirector(String director){
+		   this.director=director;
+	   }
+	   public  String getdirectior(){
+		   return director;
+	   }
+	   public void setgenre(String genre){
+		   this.genre=genre;
+	   }
+	   public String genre(){
+		   return genre;
+	   }
+	   public void setyearmade(int yearmade){
+		   this.yearmade=yearmade;
+	   }
+	   public int getyearmade(){
+		   return yearmade;
+	   }
+	}
